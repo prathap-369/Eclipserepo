885d8675df82cae277963c1ad824eda3f3943bbd
prathap <jvprathap.2024@gmail.com>
2025-03-04 16:27:22 +0530

    yeah commit

---
+++ Gitprject/src/com/flm/prp/Myclass.java
@@ -0,0 +1,17 @@
+package com.flm.prp;
+
+public class Myclass {
+	
+	private void sysout1() {
+		// TODO Auto-generated method stub
+         System.out.println("hlooooos"+"jjj");
+	}
+	public static void main(String[] args) {
+		int n= 78;
+		
+		System.out.println(n%2==0);
+		
+	}
+	  
+
+}
